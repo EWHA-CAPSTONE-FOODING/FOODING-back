abc6fc3f7c87e2dd07ac42e5e01bc47be260b4b4
ihyun <moonihyun@gmail.com>
2025-03-10 14:30:34 +0900

    first commit

---
+++ src/main/java/fooding/foodingback/FoodingBackApplication.java
@@ -0,0 +1,13 @@
+package fooding.foodingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodingBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fooding/foodingback/FoodingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fooding.foodingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
